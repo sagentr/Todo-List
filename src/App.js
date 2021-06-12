62993800b5b71065a264dd094534cfd19ab78e84
Sarah Gentry <sagentr5@gmail.com>
2021-06-11 19:18:30 -0500

    Added function to button handler and background overlay

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text = 'Learn React'/>
+      <Todo text = 'Master React'/>
+      <Todo text = 'Explore Entire Course'/>
+    </div>
+  );
+}
+
+export default App;
